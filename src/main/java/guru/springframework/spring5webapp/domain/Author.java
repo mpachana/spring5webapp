6ff6f565ba1860d0a7a827c70aa944075adbcb49
mpachana <michal.pachana@gmail.com>
2021-09-12 19:51:54 +0200

    added controllers and thymeleaf

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -47,7 +47,6 @@ public class Author {
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", books=" + Books +
                 '}';
     }
 
